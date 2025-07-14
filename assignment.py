bdd9fb8b5d31a9b23c6b28dd250910f04627ddaf
Mihir <Mihir@crc-dot1x-nat-10-239-211-38.bu.edu>
2025-07-13 20:18:48 -0400

    Finish mean_loop and median_loop

--- assignment.py
+++ assignment.py
@@ -29,19 +29,79 @@ def mean_loop(nums: Iterable[float]) -> float:
     Returns:
         The mean of the inputs.
     """
-    raise NotImplementedError
+    total = 0.0
+    nums = _validate_numeric(nums)
+    length = len(nums)
+    if length == 0:
+        raise ValueError
+
+    else:
+        for i in range(length):
+            total += nums[i]
+        return total/length
+
+
 
 
 def median_loop(nums: Iterable[float]) -> float:
     """Return the median value without using `statistics` or `numpy`."""
-    raise NotImplementedError
+    
+    nums = _validate_numeric(nums)
+    length = len(nums)
+    if length == 0:
+        raise ValueError
+    nums.sort()
+    if(length %2 == 0):
+        half = length//2
+        sumOne = nums[half -1]
+        sumTwo = nums[half]
+        total = sumOne + sumTwo
+        return total/2
+    else:
+        half = (length//2)
+        return nums[half]
+   
 
 
 def summary_stats(nums: Iterable[float]) -> Dict[str, float]:
     """Return mean, median, min, and max as a dictionary."""
-    raise NotImplementedError
+    collection = {}
+    nums = _validate_numeric(nums)
+    length = len(nums)
+    if(length == 0):
+        raise ValueError
+    else:
+        nums.sort()
+        mean = mean_loop(nums)
+        collection["mean"] = mean
+        median = median_loop(nums)
+        collection["median"] = median
+        collection["min"] = nums[0]
+        collection["max"] = nums[length - 1]
+        return collection
+
+
+
 
 
 def quantile(nums: Iterable[float], q: float) -> float:
     """Compute the *q*‑quantile (0 ≤ q ≤ 1) via linear interpolation."""
-    raise NotImplementedError
+    nums = _validate_numeric(nums)
+    length = len(nums)
+    if(length == 0):
+        raise ValueError
+    
+    else:
+        nums.sort()
+        index = (length - 1) * q
+        print(index)
+        if index == int(index):
+            index = int(index)
+            return nums[index]
+        else:
+            lower = int(index)
+            higher = min(lower + 1, length - 1)
+            fraction = (length - 1) * q - lower
+            return (1-fraction) * nums[lower] + fraction * nums[higher]
+
+
